5edb7c99fdf03e0c899fe699f2edef31a81a3cb3
Mustafa2 <Mustafauyar91@gmail.com>
2024-04-20 14:24:49 +0300

    ...

--- OptimizasyonProblemleri/problem1.jl
+++ OptimizasyonProblemleri/problem1.jl
@@ -2,7 +2,7 @@ using JuMP
 using Plots
 using Ipopt
 
-# toplamları 40 olan iki pozitif tamsayının kareleri toplamı min kaçtır
+# toplamları 40 olan iki pozitif tamsayının kareleri toplamı en düşük kaçtır ?
 
 model = Model(Ipopt.Optimizer)
 
@@ -20,3 +20,27 @@ value(x)
 value(y)
 
 objective_value(model)
+
+# y = 40 - x
+
+
+# y^2 = 1600 - 80x + x^2
+
+#y^2 + x^2 = 1600 -80x + 2x^2
+
+
+# f(x) = 1600 - 80x + 2x^2
+
+f(x) = 1600 - 80x + 2*(x^2)
+
+x = 10:30
+
+plotlyjs(size=(1366,768))
+
+p = plot(x, f,
+    title = "x² + y² func grafiği",
+    xlabel = "x değerleri",
+    ylabel = "x² + y² değerleri"
+)
+
+savefig(p,"x+y=40; min(x²+y²).svg")
